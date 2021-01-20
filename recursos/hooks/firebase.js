d770aa2ff8d7a198641cfcf566fc3fd9fcabb95f
Jorge Alberto Perez <jperez@yourappland.com>
2021-01-19 23:26:21 -0600

    modify when saving user token

--- recursos/hooks/firebase.js
+++ recursos/hooks/firebase.js
@@ -3,25 +3,19 @@ import { firebase } from "../utils/firebase";
 const db = firebase.firestore();
 
 const saveTokenPhone = async (token, uid) => {
-  let phoneTokens = [];
   try {
+    console.log(token, uid);
     const PhoneTokenRef = db
       .collection("usuarios")
       .doc(uid)
       .collection("phoneTokens");
 
-    const tokens = await PhoneTokenRef.where("phoneToken", "==", token).get();
-
-    tokens.forEach((doc) => {
-      phoneTokens.push({ ...doc.data() });
+    await PhoneTokenRef.add({
+      phoneToken: token,
     });
-    if (phoneTokens.length === 0) {
-      await PhoneTokenRef.add({
-        phoneToken: token,
-      });
-      console.log("Exito");
-    }
+    console.log("Exito");
   } catch (error) {
+    console.log(error);
     console.log("Error");
   }
 };
